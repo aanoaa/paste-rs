715b44768f90727131ebde1839d728dcf955cd4a
Hyungsuk Hong <aanoaa@gmail.com>
2022-12-02 20:24:20 +0900

    read config from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io::Result, path::PathBuf};
+
+use paste_rs::{config::Config, CONFIG_ENV};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    dotenvy::dotenv().ok();
+    env_logger::init();
+    let config_path = match env::var(CONFIG_ENV).ok() {
+        Some(path) => PathBuf::from(path),
+        None => PathBuf::from("config.toml"),
+    };
+
+    let config = Config::from(&config_path).expect("config read fail");
+    log::trace!("{:#?}", config);
+    Ok(())
 }
