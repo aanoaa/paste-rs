933f460ad8a14da50068c292e3d3313296a5cbdb
Hyungsuk Hong <aanoaa@gmail.com>
2022-12-04 15:42:05 +0900

    discover Hostname via X-Forwarded-* header first

--- src/lib.rs
+++ src/lib.rs
@@ -8,29 +8,15 @@ pub const CONFIG_ENV: &str = "CONFIG";
 
 /// template landing page content
 pub const LANDING_PAGE: &str = r#"
-  API USAGE
+  USAGE
 
       POST :HOST:
 
           Send the raw data along. Will respond with a link to the paste.
 
-          If the response code is 201 (CREATED), then the entire paste was
-          uploaded. If the response is 206 (PARTIAL), then the paste exceeded
-          the server's maximum upload size, and only part of the paste was
-          uploaded. If the response code is anything else, an error has
-          occurred. Pasting is heavily rate limited.
-
       GET :HOST:/<id>
 
           Retrieve the paste with the given id as plain-text.
-
-      GET :HOST:/<id>.<ext>
-
-          Retrieve the paste with the given id. If ext is a known code file
-          extension, the paste is syntax highlighted and returned as HTML. If
-          ext is a known file extension, the paste is returned with the
-          extension's corresponding Content-Type. Otherwise, the paste is
-          returned as plain text.
 
       DELETE :HOST:/<id>
 
@@ -59,5 +45,4 @@ pub const LANDING_PAGE: &str = r#"
               local file=${1:-/dev/stdin}
               curl --data-binary @${file} :HOST:
           }
-
 "#;
--- src/server.rs
+++ src/server.rs
@@ -8,10 +8,37 @@ use crate::LANDING_PAGE;
 
 #[get("/")]
 async fn index(req: HttpRequest) -> HttpResponse {
+    const FORWARDED_HEADER_PREFIX: &str = "x-forwarded-";
+
     let headers = req.headers();
-    let host = headers.get(header::HOST).unwrap().to_str().unwrap();
+    let mut host: Vec<&str> = Vec::new();
+    for word in ["protocol", "host"] {
+        match headers.get(format!("{}{}", FORWARDED_HEADER_PREFIX, word)) {
+            Some(header_value) => match header_value.to_str() {
+                Ok(value) => {
+                    host.push(value);
+                }
+                Err(e) => {
+                    log::trace!("unexpected header value: {e}");
+                    host.clear();
+                    break;
+                }
+            },
+            None => {
+                host.clear();
+                break;
+            }
+        }
+    }
+
+    let hostname = if host.is_empty() {
+        String::from(headers.get(header::HOST).unwrap().to_str().unwrap())
+    } else {
+        host.join("://")
+    };
+
     let template = LANDING_PAGE;
-    let content = template.replace(":HOST:", host);
+    let content = template.replace(":HOST:", &hostname);
     HttpResponse::Ok()
         .content_type(ContentType::plaintext())
         .body(content)
